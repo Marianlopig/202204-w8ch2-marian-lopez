592dc109bdcb92de177012ca171cd321d3c0eed7
Marian <marianlopig@gmail.com>
2022-05-26 23:08:02 +0200

    added robot constructor with name creator

---
+++ src/Robot/Robot.ts
@@ -0,0 +1,30 @@
+import { getRandomLetter, getRandomNumber } from "../utils/RandomUtils";
+
+type RobotType = "cleaner" | "waiter" | "developer";
+
+interface Robot {
+  name: string;
+  type: RobotType;
+  battery: number;
+  reset: () => void;
+  work: () => void;
+}
+
+class RobotImpl implements Robot {
+  name;
+  type;
+  battery;
+  constructor(type: RobotType) {
+    this.name = this.getRandomName();
+    this.type = type;
+    this.battery = 100;
+  }
+  reset() {}
+  work() {}
+
+  getRandomName(): string {
+    return getRandomLetter(3) + getRandomNumber(2);
+  }
+}
+
+export default RobotImpl;
